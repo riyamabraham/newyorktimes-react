7cd24076358233cf1142febdeb49f4fde7f56c93
Riya Abraham <riyamaryabraham12@gmail.com>
2018-08-09 16:01:56 -0500

    files

---
+++ README.md
@@ -0,0 +1,11 @@
+# NYT-react
+
+
+This app uses the full MERN stack to create a React-based rendition of the New York Times search engine. User has the ability to search for articles, save and delete them.
+
+- MongoDB 
+- Express 
+- React  
+- Node
+
+This app is live at https://scrape-react.herokuapp.com/
--- server.js
+++ server.js
@@ -1,24 +1,94 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-// Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
+var Article = require('./models/article.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+//app.use(logger('dev'));
 app.use(bodyParser.json());
-// Serve up static assets (usually on heroku)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+//app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("app/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect('mongodb://localhost/nyt-react');
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+//mongoose.connect('mongodb://heroku_rkhhrkk9:ltf4sou76eu5sb77s0cronvvb8@ds031792.mlab.com:31792/heroku_rkhhrkk9');
+// mongodb://<dbuser>:<dbpassword>@ds047592.mlab.com:47592/heroku_t60d2qtp
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
 });
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+})
+
+app.get('/api/saved', function(req, res) {
+
+  Article.find({})
+    .exec(function(err, doc){
+
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    })
+});
+
+app.post('/api/saved', function(req, res){
+
+  var newArticle = new Article({
+    title: req.body.title,
+    date: req.body.date,
+    url: req.body.url
+  });
+
+  newArticle.save(function(err, doc){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      res.json(doc);
+    }
+  });
+
+});
+
+app.delete('/api/saved/:id', function(req, res){
+
+  Article.find({'_id': req.params.id}).remove()
+    .exec(function(err, doc) {
+      res.send(doc);
+  });
+
+});
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+
+
+
